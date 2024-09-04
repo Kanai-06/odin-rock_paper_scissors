6e5615575bdd104cf06936306fcd3bfa7a081c5a
Kanai-06 <153616932+Kanai-06@users.noreply.github.com>
2024-09-04 14:44:53 +0200

    Fully working website

--- script.js
+++ script.js
@@ -8,24 +8,16 @@ function getComputerChoice() {
     return choicesArray[randomNumber(3)];
 }
 
-function getUserChoice() {
-    return prompt("Which attack ?");
-}
-
-function playRound() {
-    let ComputerChoice= getComputerChoice();
-    let UserChoice= getUserChoice().toLowerCase();
+function playRound(UserChoice) {
+    let ComputerChoice = getComputerChoice();
 
     if (ComputerChoice == UserChoice) {
-        console.log(`Draw ! You : ${UserChoice} --- Bot : ${ComputerChoice}`);
         return "D";
     } else if ((UserChoice == "rock" && ComputerChoice == "scissors") || 
     (UserChoice == "scissors" && ComputerChoice == "paper") || 
     (UserChoice == "paper" && ComputerChoice == "rock")){
-        console.log(`Win ! You : ${UserChoice} --- Bot : ${ComputerChoice}`);
         return "W";
     } else {
-        console.log(`Lose ! You : ${UserChoice} --- Bot : ${ComputerChoice}`);
         return "L";
     }
 }
@@ -34,20 +26,52 @@ function playGame() {
     let ComputerScore= 0;
     let UserScore= 0;
 
-    while((ComputerScore < 5) && (UserScore < 5)) {
-        let result= playRound();
-        if (result== "W") {
-            UserScore+= 1;
-        } else if (result== "L") {
-            ComputerScore+= 1;
+    const score = document.querySelector("#score-text");
+    const UserCards = document.querySelector("#UserChoice");
+    
+    const user = document.querySelector(".user");
+    const bot = document.querySelector(".bot");
+
+    user.classList.remove("winner");
+    bot.classList.remove("winner");
+    const message = document.querySelector("#message");
+
+    score.textContent = `${UserScore} - ${ComputerScore}`; 
+    message.textContent = ""; 
+    
+    let UserChoice;
+
+    UserCards.addEventListener("click", handleRound);
+    
+    function handleRound(e){
+        if(ComputerScore < 5 && UserScore < 5){
+            UserChoice = e.target.closest('div').id;
+
+            let result= playRound(UserChoice);
+            if (result== "W") {
+                UserScore+= 1;
+            } else if (result== "L") {
+                ComputerScore+= 1;
+            }
+
+            score.textContent = `${UserScore} - ${ComputerScore}`;           
         }
 
-        alert(`Score : You= ${UserScore} --- Bot= ${ComputerScore}`);
-    }
-
-    if (ComputerScore== 5) {
-        return `Computer won the game ${ComputerScore} to ${UserScore}`;
-    } else {
-        return `You won the game ${ComputerScore} to ${UserScore}`;
+        if (ComputerScore== 5) {
+            message.textContent = "You lost, CHEH !";
+            bot.classList.add("winner");
+            UserCards.removeEventListener("click", handleRound);
+            
+        } else if(UserScore== 5){
+            message.textContent = "You win !";
+            user.classList.add("winner");
+            UserCards.removeEventListener("click", handleRound);
+        }
     }
 }
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () =>{
+    playGame();
+});
